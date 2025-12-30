c4401697db73acf8bd93248d7dd02a77203b7d84
julianajardimpereira <jujujardim@gmail.com>
2025-12-30 15:28:34 -0300

    J

--- app2.py
+++ app2.py
@@ -19,7 +19,7 @@ Contexto:
 Você é um especialista em Inteligência Artificial.
 
 Objetivo:
-Extrair termos para indexação a partir de uma pergunta em linguagem natural, garantindo precisão terminológica, rastreabilidade e reprodutibilidade, conforme exigido em ambientes científicos.
+Extrair termos e os traduzir para indexação a partir de uma pergunta em linguagem natural, garantindo precisão terminológica, rastreabilidade e reprodutibilidade, conforme exigido em ambientes científicos.
 
 Restrições obrigatórias:
 Os termos devem constar no vocabulário controlado fornecido (ex.: tesauro, ontologia, taxonomia científica).
@@ -32,6 +32,8 @@ Formato da saída:
 Retornar exclusivamente uma lista JSON.
 Manter a grafia exata conforme definida no vocabulário controlado.
 Não incluir metadados, justificativas ou texto explicativo após a resposta.
+Leve em consideração que as palavras-chave estão em inglês. Traduza as palavras-chave e as apresente apenas na língua portuguesa.
+
 VOCABULÁRIO:
 {contextos}
 
@@ -56,10 +58,10 @@ RESPOSTA:
 
 
 # === 5. Teste ===
-responder("Clustering é uma técnica não supervisionada de aprendizagem de máquina?")
+responder("Name a chatbot similar to BARD.")
 responder("O que é Big Data?")
 responder("What is Big Data?")
-responder("O que é A3t-Gcn")
-responder("Defina Detecção de Linguagem Abusiva.")
-responder("Quais são os Modelos Abstrativos presentes na IA?")
+responder("What is A3t-Gcn")
+responder("Define Abusive Language Detection.")
+responder("What are the Abstractive Models present in AI?")
 
