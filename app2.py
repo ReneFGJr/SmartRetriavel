08ef4ea33af9304afb68782cf48bfd728b29c954
julianajardimpereira <jujujardim@gmail.com>
2025-12-30 18:13:38 -0300

    J

--- app2.py
+++ app2.py
@@ -64,4 +64,3 @@ responder("What is Big Data?")
 responder("What is A3t-Gcn")
 responder("Define Abusive Language Detection.")
 responder("What is the Abstractive Model present in Artificial Intelligence?")
-
